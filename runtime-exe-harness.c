023779e55eebda312b6e3a185b3310cec998f1a9
Jevin Sweval <jevinsweval@gmail.com>
2025-03-14 15:12:56 -0400

    cool

--- runtime-exe-harness.c
+++ runtime-exe-harness.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <mach/ndr.h>
 
+extern void dylibify_entry(void);
+
 int main(void) {
     printf("&fopen: %p\n", &fopen);
+    printf("&dylibify_entry: %p\n", &dylibify_entry);
     printf("&NDR_record: %p\n", &NDR_record);
     printf("&NDR_record.reserved1: %p\n", &NDR_record.reserved1);
     printf("NDR_record.reserved1 before: %d\n", NDR_record.reserved1);
     NDR_record.reserved1 = 7;
     printf("NDR_record.reserved1 after: %d\n", NDR_record.reserved1);
+    printf("calling dylibify_entry bye bye world\n");
+    fflush(stdout);
+    dylibify_entry();
     return 0;
 }
